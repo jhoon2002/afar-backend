8e2d3c78308e470ba83ca6e367ba7a141a4719c1
jhoon <jhoon2002@naver.com>
2021-03-04 17:25:26 +0900

    office

--- routes/posts.js
+++ routes/posts.js
@@ -7,25 +7,83 @@ let Employee = require("../models/employee.js")
 페이징: /api/posts?page=1&itemsPerPage=10&sortBy[]=created&sortDesc[]=true&mustSort=false&multiSort=false
 */
 
-let count = function(req, res, next) {
+async function countAll(filter) {
+    return await Post.countDocuments(filter).exec()
+}
+
+async function find(sort, skip, limit) {
+    return await Post.find(filter,
+            {
+                boardId: 1,
+                subject: 1,
+                userId: 1,
+                created: 1
+            }
+        )
+        .sort(sort)
+        .skip(skip)
+        .limit(limit)
+        .exec()
+}
+
+async function getNames(set) {
+    return await Employee.find(
+        { userId: { $in: Array.from(set) } },
+        { userId: 1, name: 1}
+        ).exec()
+}
+
+async function getAlldata(req) {
 
     let page = req.query.page * 1
     let limit = req.query.itemsPerPage * 1
 
+    let sort = {}
+    if (req.query.sortBy) {
+        for (let [index, value] of req.query.sortBy.entries()) {
+            if (index == 0) sort = {}
+            let field = value
+            let desc = req.query.sortDesc[index] == "true" ? -1 : 1
+            sort[field] = desc
+        }
+    }
+
     let filter = {}
     filter["boardId"] = req.query.boid ? req.query.boid : { '$exists': true }
 
-    Post.countDocuments( filter ).then( (cnt) => {
-        let totalPage = parseInt((cnt - 1) / limit) + 1
-        let skip = (page - 1) * limit
-        console.log(page, limit, cnt, totalPage, skip)
-    })
-    next()
+    let cnt = await countAll(filter)
+
+    let totalPage = parseInt( (cnt - 1) / limit ) + 1
+    let skip = ( page - 1 ) * limit
+
+    let ret = await find(sort, skip, limit)
+
+    let set = new Set()
+    for ( let item of ret ) {
+        set.add(item.userId)
+    }
+
+    let arr = await getNames(set)
+    let names = {}
+    for (let d of arr) {
+        names[d.userId] = d.name
+    }
+
+    return {
+        page: page,
+        totalPage: totalPage,
+        sort: sort,
+        skip: skip,
+        limit: limit,
+        items: ret,
+        names: names
+    }
 }
 
-router.get('/', count, function(req, res, next) { //목록
-    console.log("===========")
-    res.send("-------------")
+router.get('/', function (req, res, next) { //목록
+
+    getAlldata(req).then( res.send )
+
 })
 
 module.exports = router
